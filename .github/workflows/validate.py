8c52c52c877eccd0d7f433c2367ed09bbcddbca1
kapil.alawadhi <kapil.alawadhi@gmail.com>
2023-07-21 13:41:15 +0530

    changes

--- .github/workflows/validate.py
+++ .github/workflows/validate.py
@@ -1,5 +1,8 @@
+import sys
+
 def main():
     print("Hello from action")
+    print('Argument List:', str(sys.argv))
     # raise Exception("Sorry, issue with the current code snip")
 
 if __name__== '__main__':
--- utility.py
+++ utility.py
@@ -1,6 +1,6 @@
 # This function adds two numbers
-def add(x_29, y):
-    return x_29 + y
+def add(x_30, y):
+    return x_30 + y
 
 # This function subtracts two numbers
 def subtract(x, y):
