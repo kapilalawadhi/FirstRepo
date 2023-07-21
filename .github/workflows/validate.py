1294ec38ccf42e34aba99d7dc4516a51031f16ad
kapil.alawadhi <kapil.alawadhi@gmail.com>
2023-07-21 17:02:24 +0530

    changes

--- .github/workflows/validate.py
+++ .github/workflows/validate.py
@@ -3,6 +3,10 @@ import sys
 def main():
     print("Hello from action")
     print('Argument List:', str(sys.argv))
+    with open(str(sys.argv)) as f:
+        lines = f.readlines()
+        print(lines)
+
     # raise Exception("Sorry, issue with the current code snip")
 
 if __name__== '__main__':
