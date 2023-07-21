e6f249f149604b7efb7e7167897019f50e07322d
kapil.alawadhi <kapil.alawadhi@gmail.com>
2023-07-21 17:11:57 +0530

    changes

--- .github/workflows/validate.py
+++ .github/workflows/validate.py
@@ -3,8 +3,7 @@ import sys
 def main():
     print("Hello from action")
     print('Argument List:', str(sys.argv))
-    print('Argument List[1]:', str(sys.argv[1]))
-    with open(str(sys.argv[1])) as f:
+    with open('utility.py') as f:
         lines = f.readlines()
         print(lines)
 
