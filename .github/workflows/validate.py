e3457b6873dee7b3ce8769dc25f2db08efbf24a8
kapil.alawadhi <kapil.alawadhi@gmail.com>
2023-07-24 10:03:02 +0530

    changes

--- .github/workflows/validate.py
+++ .github/workflows/validate.py
@@ -6,11 +6,15 @@ def main():
     if len(sys.argv) >0:
         changed_files = sys.argv[1]
         print('changed files are->',changed_files)
-        for item in sys.argv[1].split(","):
-            print("filename->")
-            with open(item) as f:
-                lines = f.readlines()
-                print(lines)
+        for changed_file in sys.argv:
+            if changed_file.__contains__("/workflows/"):
+                continue
+            else:
+                for file_name in changed_file.split(","):
+                    print("filename->",file_name)
+                    with open(file_name) as f:
+                        lines = f.readlines()
+                        print(lines)
 
     # raise Exception("Sorry, issue with the current code snip")
 
