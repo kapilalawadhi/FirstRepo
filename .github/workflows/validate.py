471b6ddefb9327f0f832da266582602ffb463bce
kapil.alawadhi <kapil.alawadhi@gmail.com>
2023-07-24 12:31:18 +0530

    changes

--- .github/workflows/validate.py
+++ .github/workflows/validate.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 def main():
     print("Hello from action")
@@ -6,6 +7,7 @@ def main():
     if len(sys.argv) >0:
         changed_files = sys.argv[1]
         print('changed files are->',changed_files)
+        errors=''
         for changed_file in sys.argv:
             if changed_file.__contains__("/workflows/"):
                 continue
@@ -13,10 +15,24 @@ def main():
                 for file_name in changed_file.split(","):
                     print("filename->",file_name)
                     with open(file_name) as f:
-                        lines = f.readlines()
-                        print(lines)
+                        full_line = f.readlines()
+                        txt = "The rain in Spain"
+                        check_secret_key = re.findall("([a-zA-Z0-9+/]{40})", full_line)
+                        if len(check_secret_key) > 0:
+                            print("keyfound->",check_secret_key)
+                            errors =  errors + "/n" + "secret_key found in file " + file_name
+                        # if full_line.__contains__("="):
+                        #     while len(full_line.split("="))>0:
+                        #         var_value = full_line.split("=")[1]
+                        #         # Validate for secret key using regular exp
+                        #         if var_value.__contains__("="):
+                        #             full_line=var_value
 
-    # raise Exception("Sorry, issue with the current code snip")
+
+
+                        print(lines)
+    if len(errors)>0:
+        raise Exception("Sorry, issue with the current code snip->/n",errors)
 
 if __name__== '__main__':
     main()
--- utility.py
+++ utility.py
@@ -1,3 +1,10 @@
+geeks = 1
+Geeks = 2
+Ge_e_ks = 5
+_geeks = 6
+geeks_ = 7
+_GEEKS_ = 8
+
 # This function adds two numbers
 def add(x_38, y):
     return x_38 + y + 0
